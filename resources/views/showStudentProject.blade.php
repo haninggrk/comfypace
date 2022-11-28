d9700b75b8ee22d67cdfce744dfc80243ab0051b
galih <haning.grk@gmail.com>
2022-11-28 10:16:43 +0700

    a

--- resources/views/showStudentProject.blade.php
+++ resources/views/showStudentProject.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-guest-layout>
     <x-slot name="header">
      <a href="{{url()->previous()}}"> <button type="button" class="inline-block mr-2 items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Back</button>
      </a><h2 class="font-semibold inline-block   text-xl text-gray-800 leading-tight">
